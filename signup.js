a2e4382317bf9b728defd5e3d436aac9828f7cfa
Joud676 <139700859+Joud676@users.noreply.github.com>
2025-03-15 02:12:51 +0000

    Deploying to gh-pages from @ Joud676/DroneClub_Ramadan@3ff9b3b4229b44b6d1aabf7471df68887b1bb149 🚀

--- signup.js
+++ signup.js
@@ -2,17 +2,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebas
 import { getFirestore, doc, setDoc, getDoc, collection, query, where, getDocs } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "FIREBASE_API_KEY",
-  authDomain: "FIREBASE_AUTH_DOMAIN",
-  projectId: "FIREBASE_PROJECT_ID",
-  storageBucket: "FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-  appId: "FIREBASE_APP_ID",
+  apiKey: "AIzaSyDdDcXqRLypztFUskDHx3DD-eXqRSeM9j8",
+  authDomain: "ramadan-7647a.firebaseapp.com",
+  projectId: "ramadan-7647a",
+  storageBucket: "ramadan-7647a.firebasestorage.app",
+  messagingSenderId: "572887923466",
+  appId: "1:572887923466:web:d5b3555a20ebf6b79658f3",
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 document.getElementById("signup-form").addEventListener("submit", async (e) => {
     e.preventDefault();
 
